709d5dab21918c2c6e35dc7e315308d15e5e2688
Alex Wolf <alexrudiwolf@gmail.com>
2017-11-09 22:41:13 -0500

    end

--- app/src/main/java/com/example/alexr/ideamanager/services/IdeaService.java
+++ app/src/main/java/com/example/alexr/ideamanager/services/IdeaService.java
@@ -5,8 +5,12 @@ import com.example.alexr.ideamanager.models.Idea;
 import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface IdeaService {
     @GET("ideas")
     Call<List<Idea>> getIdeas();
+
+    @GET("ideas/{id}")
+    Call<Idea> getIdea(@Path("id")int id);
 }
